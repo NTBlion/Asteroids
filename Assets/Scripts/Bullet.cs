101858c9d7a8c293fbf3fabcb1af66b10e5a2a31
NTBlion <103928473+NTBlion@users.noreply.github.com>
2024-07-10 21:13:28 +0300

    BulletFix

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,27 +1,33 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private Rigidbody2D _rigidbody;
-    
+    [SerializeField] private float _bulletLifeTime;
+
     private Pool<Bullet> _pool;
+    private float _time;
 
     public void Init(Pool<Bullet> pool)
     {
         _pool = pool;
     }
-    
-    private void FixedUpdate()
+
+    private void Update()
     {
-        _rigidbody.AddForce(transform.up * _speed, ForceMode2D.Impulse);
+        _time += Time.deltaTime;
+        transform.Translate(Vector2.up * _speed * Time.deltaTime);
+
+        if (_time >= _bulletLifeTime)
+        {
+            _time = 0;
+            _pool.Disable(this);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+        _time = 0;
         _pool.Disable(this);
     }
 }
